03f1df725b2e7b960a9079ecb83316b0736cfb22
Vi-The <91126091+Vi-The@users.noreply.github.com>
2023-05-11 13:27:05 +0200

    gg
    kunne ikke lige finde ud af at få det til at hænge samme men tror jeg er tæt på

--- CP/src/main/java/APIController.java
+++ CP/src/main/java/APIController.java
@@ -1,3 +1,7 @@
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -9,6 +13,9 @@ import java.net.http.HttpResponse;
 public class APIController {
 
     static String api_key = "fbdd9b85653960b2412a4465f433454a"; // security risk
+    private static final String SERVER_URL = "https://v3.football.api-sports.io/standings?league=39&season=2019&team=";
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final OkHttpClient client = new OkHttpClient();
 
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 
@@ -16,10 +23,10 @@ public class APIController {
 
    public static void check(String team) throws IOException, InterruptedException, URISyntaxException {
        HttpClient client = HttpClient.newHttpClient();
-       
+
 
        HttpRequest request = HttpRequest.newBuilder()
-               .uri(new URI("https://v3.football.api-sports.io/standings?league=39&season=2019&team=" + team))// + value = GUI
+               .uri(new URI(SERVER_URL + team))// + value = GUI
                .header("x-rapidapi-key", api_key)
                .header("x-rapidapi-host", "v3.football.api-sports.io")
                .GET()
@@ -50,5 +57,40 @@ public class APIController {
 
 
    }
+    public static String handlePostRequest(String json) throws IOException, JSONException {
+//String json = "{\"State\":1,\"Program name\":\"MoveToAssemblyOperation\"}";
+        System.out.println("landet");
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(SERVER_URL )
+                .put(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        //System.out.println(response.body().string());
+
+        String responseBody = response.body().string();
+        //String cleanedInput = responseBody.replaceAll("[{}]", "");
+
+
+        JSONObject jsonObject = new JSONObject(responseBody);
+
+        String teamName = jsonObject.getString("name");
+        String liga = jsonObject.getString("group");
+        int year = jsonObject.getInt("season");
+        int points = jsonObject.getInt("points");
+        int position = jsonObject.getInt("rank");
+
+        System.out.println("Team name: " + teamName);
+        System.out.println("League" + liga);
+        System.out.println("Year: " + year);
+        System.out.println("Points: " + points);
+        System.out.println("position: " + position);
+
+        //DBController.setData();
+
+
+        return json;
+    }
    }
 
