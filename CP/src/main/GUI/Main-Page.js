65f90e3801c49465d9abeb3958ce5c1ec45b1c4d
Vi-The <91126091+Vi-The@users.noreply.github.com>
2023-05-14 02:53:06 +0200

    fuck det hele
    klokken er 3 jeg har fundet intet

--- CP/src/main/GUI/Main-Page.js
+++ CP/src/main/GUI/Main-Page.js
@@ -24,3 +24,50 @@ function loadData() {
     xhttp.send();
 }
 
+
+function fetchDataFromDatabase() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:8001/data', true);
+    xhr.onload = function() {
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            // Pass the data to the function that creates the chart
+            createChart(data);
+        }
+    };
+    xhr.send();
+}
+function createChart(data) {
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+        // Create the data table.
+        const dataTable = new google.visualization.DataTable();
+        dataTable.addColumn('string', 'Team');
+        dataTable.addColumn('number', 'Wins');
+
+        // Add data to the table
+        data.forEach(function(team) {
+            dataTable.addRow([teamName, points]);
+        });
+
+        // Set chart options
+        const options = {
+            'title': 'Team Wins',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw the chart, passing in the data and options.
+        const chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(dataTable, options);
+    }
+}
